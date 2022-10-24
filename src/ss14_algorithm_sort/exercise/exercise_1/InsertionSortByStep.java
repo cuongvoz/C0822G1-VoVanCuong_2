287b48041950bd63d24689be9ee50cbb6725be3e
cuongvoz <mainvov@gmail.com>
2022-10-24 15:52:15 +0700

    nop bai tap

--- src/ss14_algorithm_sort/exercise/exercise_1/InsertionSortByStep.java
+++ src/ss14_algorithm_sort/exercise/exercise_1/InsertionSortByStep.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 public class InsertionSortByStep {
 
     public static void main(String[] args) {
-        int[] array = {9,8,7,6,10,11,1};
+        int[] array = {9, 8, 7, 6, 10, 11, 1};
         int step = 1;
         System.out.println("Mảng lúc đầu là " + Arrays.toString(array));
         int i;
         for (i = 1; i < array.length; i++) {
             int key = array[i];
             int j;
-            for (j = i -1; j >= 0 && array[j] > key; j--) {
+            for (j = i - 1; j >= 0 && array[j] > key; j--) {
                 array[j + 1] = array[j];
             }
             if (i != j) {
-                if (array[j+1] == key) {
-                    System.out.println("Bước " + step + ": vì " + array[j+1] + " lơn hơn " + array[j] + " nên giữ nguyên");
+                if (array[j + 1] == key) {
+                    System.out.println("Bước " + step + ": vì " + array[j + 1] + " lơn hơn " + array[j] + " nên giữ nguyên");
                     step++;
                 } else {
-                    System.out.print("Bước " + step + ": chèn " + key + " vào trước " + array[j+1]);
+                    System.out.print("Bước " + step + ": chèn " + key + " vào trước " + array[j + 1]);
                     array[j + 1] = key;
                     System.out.println(" mảng lúc này là " + Arrays.toString(array));
                     step++;
